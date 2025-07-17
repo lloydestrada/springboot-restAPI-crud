6c95ac7122363624028e18af21a2e47bcc85a37b
lloydestrada <iamlloydestrada@gmail.com>
2025-07-17 20:02:48 +0800

    add Update employee

--- src/main/java/com/lloyd/restAPICrudDemo/rest/EmployeeRestController.java
+++ src/main/java/com/lloyd/restAPICrudDemo/rest/EmployeeRestController.java
@@ -5,9 +5,7 @@ package com.lloyd.restAPICrudDemo.rest;
 import com.lloyd.restAPICrudDemo.entity.Employee;
 import com.lloyd.restAPICrudDemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,4 +27,40 @@ public class EmployeeRestController {
 
         return employeeService.findAll();
     }
+
+    //add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+
+        Employee theEmployee = employeeService.findId(employeeId);
+
+        if(theEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return theEmployee;
+    }
+
+    //add a mapping for POST/ADD /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        //set id to 0
+        //NOTE: this is to force a save a new item - instead of updating
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    //add mapping for PUT /employees - update an existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
 }
