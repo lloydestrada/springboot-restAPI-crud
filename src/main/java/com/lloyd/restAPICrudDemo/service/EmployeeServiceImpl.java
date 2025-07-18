80c58f257237cbf9e30c4a39547ca4324c657fa0
lloydestrada <iamlloydestrada@gmail.com>
2025-07-18 20:33:42 +0800

    refactor DAO to JPA

--- src/main/java/com/lloyd/restAPICrudDemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/lloyd/restAPICrudDemo/service/EmployeeServiceImpl.java
@@ -1,49 +1,69 @@
 package com.lloyd.restAPICrudDemo.service;
 
-import com.lloyd.restAPICrudDemo.DAO.EmployeeDAO;
+
+import com.lloyd.restAPICrudDemo.DAO.EmployeeRepository;
 import com.lloyd.restAPICrudDemo.entity.Employee;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     //setup constructor injection
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO){
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository){
 
-        employeeDAO = theEmployeeDAO;
+        employeeRepository = theEmployeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findId(int theId) {
-        return employeeDAO.findId(theId);
+
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee = null;
+        if(result.isPresent()){
+            theEmployee = result.get();
+        }
+        else {
+            throw new RuntimeException ("Did not find employee id: " + theId);
+        }
+
+        return theEmployee;
     }
 
-    @Transactional
     @Override
     public Employee save(Employee theEmployee) {
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
-    @Transactional
     @Override
     public void deleteById(int theId) {
-        employeeDAO.deleteById(theId);
+        employeeRepository.deleteById(theId);
     }
 
     @Override
     public Employee findById(int employeeId) {
-        return employeeDAO.findId(employeeId);
+
+        Optional <Employee> result = employeeRepository.findById(employeeId);
+
+        Employee theEmployee = null;
+        if(result.isPresent()){
+            theEmployee = result.get();
+        }
+        else {
+            throw new RuntimeException ("Did not find employee id: " + employeeId);
+        }
+        return theEmployee;
     }
 }
