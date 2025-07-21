758fa684a889ac292717dfc5a9eb850eb6370e4f
lloydestrada <iamlloydestrada@gmail.com>
2025-07-21 20:05:48 +0800

    .

--- src/main/java/com/lloyd/restAPICrudDemo/DAO/EmployeeRepository.java
+++ src/main/java/com/lloyd/restAPICrudDemo/DAO/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.lloyd.restAPICrudDemo.DAO;
 
 import com.lloyd.restAPICrudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "testmembers")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //that's all
